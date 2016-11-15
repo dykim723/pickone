cbd32e06f045b8ac3a0b9efd8cb965ff1431f4fc
dykim723 <dykim723@gmail.com>
2016-11-16 00:28:39 +0900

    파일 다운로드 가능
    # 프로젝트 root에 uploads 라는 폴더 만들어져 있어야 함.

--- server/app.js
+++ server/app.js
@@ -51,35 +51,6 @@ app.use('/users', users);
 app.use('/login', login);
 
 /** API path that will upload the files */
-app.post('/upload', function(req, res) {
-  upload(req,res,function(err){
-    if(err){
-      res.json({error_code:1,err_desc:err});
-      return;
-    }
-    res.json({error_code:0,err_desc:null});
-  })
-});
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 var storage = multer.diskStorage({ //multers disk storage settings
   destination: function (req, file, cb) {
     cb(null, './uploads/')
@@ -89,6 +60,29 @@ var storage = multer.diskStorage({ //multers disk storage settings
     cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
   }
 });
+var upload = multer({ //multer settings
+  storage: storage
+}).single('file');
+/** API path that will upload the files */
+app.post('/upload', function(req, res) {
+  upload(req,res,function(err){
+    if(err){
+      res.json({error_code:1,err_desc:err});
+      return;
+    }
+    res.json({error_code:0,err_desc:req.body});
+  })
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+
 
 
 
