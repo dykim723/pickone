96d0e91b4a950256a2a230ca6870929676f371f4
dykim723 <dykim723@gmail.com>
2016-12-06 23:57:16 +0900

    server & client 연동 작업 중 크롬에서 테스트 할 때Req <-> Res는 되는데 데이터가 없음 안드로이드 폰에서 할때 등록 버튼 누르면 앱이 죽음 $http.post 날리지를 못함

--- server/app.js
+++ server/app.js
@@ -51,33 +51,7 @@ app.use(function(req, res, next) {
 app.use('/', index);
 app.use('/users', users);
 app.use('/login', login);
-app.use('/posting', posting);
-
-/** API path that will upload the files */
-/*
- var storage = multer.diskStorage({ //multers disk storage settings
- destination: function (req, file, cb) {
- cb(null, './uploads/')
- },
- filename: function (req, file, cb) {
- var datetimestamp = Date.now();
- cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
- }
- });
- var upload = multer({ //multer settings
- storage: storage
- }).single('file');
- /!** API path that will upload the files *!/
- app.post('/upload', function(req, res) {
- upload(req,res,function(err){
- if(err){
- res.json({error_code:1,err_desc:err});
- return;
- }
- res.json({error_code:0,err_desc:req.body});
- })
- });
- */
+app.use('/postingUpload', posting);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -87,7 +61,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-
 
 
 
