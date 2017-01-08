69792242bf0ed6146272a474257e26e766fa5105
dykim723 <dykim723@gmail.com>
2017-01-08 15:37:09 +0900

    Login Activity 추가 Server로 Request 작업 중(connect failed: EHOSTUNREACH (No route to host)) 에러 뜸. 원인 확인 중

--- client_android/AudioMixer/app/src/main/java/com/team/audiomixer/audiomixer/MainActivity.java
+++ client_android/AudioMixer/app/src/main/java/com/team/audiomixer/audiomixer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team.audiomixer.audiomixer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,10 +81,14 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        Intent myIntent = null;
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            myIntent = new Intent(MainActivity.this, LoginActivity.class);
+            myIntent.putExtra("key", "Login"); //Optional parameters
+            MainActivity.this.startActivity(myIntent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
